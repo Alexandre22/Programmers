b1305f52726103bc1c8eebf4a4b84610664d6729
alexandre <alexandre_dsr@hotmail.com>
2019-01-29 00:25:07 -0200

    projetoProgrammers

---
+++ ProgrammersCRUD/ProgrammersCRUD/DAL/ProgramaDBContext.cs
@@ -0,0 +1,20 @@
+﻿using ProgrammersCRUD.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ProgrammersCRUD.DAL
+{
+    public class ProgramaDBContext : DbContext
+    {
+        public ProgramaDBContext() : base("ProgramaDBContext")
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ProgrammersCRUD/ProgrammersCRUD/Models/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ProgrammersCRUD.Models
+{
+    public class Pessoa
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
